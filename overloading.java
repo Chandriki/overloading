12b72637018b87fedab4226e7456103047863eb6
Chandriki <120454846+Chandriki@users.noreply.github.com>
2023-04-23 09:42:17 -0700

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,16 @@
+package chandriki1;
+
+public class overloading {
+	static void ff() {
+		System.out.println("good  morning");
+	}
+	static void ff(int a) {
+		System.out.println("goood morning"+a);
+	}
+	public static void main(String[] args) {
+		ff();
+		ff(22);
+	}
+
+}  
+
